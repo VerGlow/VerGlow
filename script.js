a22cf4d80b7c2db8bee03b956147003da18942dc
VerGlow <asadshedah@gmail.com>
2024-06-28 11:16:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         sections.forEach(section => {
             const sectionTop = section.offsetTop;
-            if (pageYOffset >= sectionTop - 60) {
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
                 current = section.getAttribute('id');
             }
         });
@@ -17,6 +18,19 @@ document.addEventListener('DOMContentLoaded', () => {
             if (link.getAttribute('href').includes(current)) {
                 link.classList.add('active');
             }
+        });
+    });
+
+    // Smooth scrolling
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = e.currentTarget.getAttribute('href') === "#" ? "header" : e.currentTarget.getAttribute('href').substring(1);
+            const targetPosition = document.getElementById(targetId).offsetTop;
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
         });
     });
 });
